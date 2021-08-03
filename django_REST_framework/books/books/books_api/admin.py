0038a67530a82167f4c7c101722ae18ee2f245b0
zhyordnova <zh.yordanova88@gmail.com>
2021-08-03 20:54:39 +0300

    Remove file

--- django_REST_framework/books/books/books_api/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from books.books_api.models import BookModel
-
-admin.site.register(BookModel)
--- django_REST_framework/books/books/books_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BooksApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'books.books_api'
